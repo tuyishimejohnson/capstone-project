d714ce5231245f4d5a2b617bd896438b10e468cb
tuyishime johnson <j.tuyishime4@alustudent.com>
2025-07-13 20:54:30 +0200

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { LoginForm } from "./components/auth/LoginForm";
 import { SignupForm } from "./components/auth/SignupForm";
 import { Dashboard } from "./components/dashboard/Dashboard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { PredictionUploadPage } from "./components/dashboard/PredictionModel";
-import { ActiveCases } from "./components/dashboard/ActiveCases/ActiveCases";
 
 function App() {
   return (
